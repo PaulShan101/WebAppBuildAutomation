6b4acb7a394f0a556e3ca80a9dc37c79d6a741e6
Paul Shanahan <20083717@mail.wit.ie>
2021-11-09 11:01:51 +0000

    CI - Basic pipeline.

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -84,13 +84,7 @@ export default function MovieCard({ movie, action }) {
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-        {action(movie)}
-
-       
-      <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-        <FavoriteIcon color="primary" fontSize="large" />
-    </IconButton>
-        <Link to={`/movies/${movie.id}`}>
+        {action(movie)} <Link to={`/movies/${movie.id}`}>
           <Button variant="outlined" size="medium" color="primary">
             More Info ...
           </Button>
