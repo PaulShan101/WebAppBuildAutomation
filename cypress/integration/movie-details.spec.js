35d2049cb86799b8e6d0edaa07a6dd89400d26b6
Paul Shanahan <pshaners10@gmail.com>
2021-12-08 22:35:04 +0000

    updated movie details spec

--- cypress/integration/movie-details.spec.js
+++ cypress/integration/movie-details.spec.js
@@ -1,10 +1,6 @@
-const { Card } = require("@material-ui/core");
-
 let movieId = 335983; // The movie Venom
 let movie;
 let reviews;
-
-
 
 describe("Movie Details Page", () => {
   before(() => {
@@ -26,26 +22,23 @@ describe("Movie Details Page", () => {
     it("should display movie title in the page header", () => {
       cy.get("h3").contains(movie.title);
     });
+  });
+});
    
        
-        // describe("Base tests", () => {
+        describe("Base tests", () => {
         //   it("should display movie pictures", () => {
         //     cy.get("div").contains("https://image.tmdb.org/t/p/w500"+movie.poster_path);
         //   });
     
-        //   it("should display the movie's details", () => {
-        //     cy.get("h3").contains("Overview");
-        //     cy.get("h3").next().contains(movie.overview);
-        //     cy.get("ul")
-        //       .eq(1)
-        //       .within(() => {
-        //         const genreChips = movie.genres.map((g) => g.name);
-        //         genreChips.unshift("Genres");
-        //         cy.get("span").each(($card, index) => {
-        //           cy.wrap($card).contains(genreChips[index]);
-        //       });
-        //     });
+          it("should display the movie's details", () => {
+            cy.get("h3").contains("Overview");
+            cy.get("h3").next().contains(movie.overview);
+            cy.get("ul")
+              .eq(1)
+             
         });
-        });
-      
+      });
+       
+    
     
