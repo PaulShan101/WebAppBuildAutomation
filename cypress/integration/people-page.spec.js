c028afce4b36743ccf2a19ea270a03c10d4bbd8f
Paul Shanahan <pshaners10@gmail.com>
2021-12-10 21:39:03 +0000

    Updated People page tests

---
+++ cypress/integration/people-page.spec.js
@@ -0,0 +1,58 @@
+let actor = 11969;
+let movie;
+
+beforeEach(() => {
+      cy.visit("/movies/people")
+    });
+
+    describe("check people", function()  {
+      it("visits people page", function() {
+        cy.contains("People")
+       })
+      })
+
+      describe("check Piper perabo", function()  {
+        it("visits Piper Perabo", function() {
+          cy.contains("Piper Perabo")
+         })
+        })
+
+        describe("check Acting", function()  {
+          it("checks for Acting", function() {
+            cy.contains("Acting")
+           })
+          })
+
+          // describe("check Music", function()  {
+          //   it("checks for Music", function() {
+          //     cy.contains("Music")
+          //    })
+          //   })
+   
+
+// describe("PeoplePage", () => {
+//   before(() => {
+//     cy.request(
+//         `https://api.themoviedb.org/3/person/popular?api_key=${process.env.REACT_APP_TMDB_KEY}`
+//         )
+//           .then(res => res.json())
+//           .then(json => json.results);
+//         })
+//     })
+//   beforeEach(() => {
+//     cy.visit("/movies/people")
+//   });
+  
+//   describe("Base tests", () => {
+//     it("should display actor title in the page header", () => {
+//       cy.get("div").contains(actor.name);
+//     });
+//   });
+
+//   describe("Base tests", () => {
+//       it("should display movie pictures", () => {
+//         cy.get("div").contains("https://image.tmdb.org/t/p/w500"+actor.profile_path)
+//       });
+//     });
+
+   
